49b4d74b8c8bfc93f9cb75ee6af8541bec7327bd
amirhosein <ah.t222amir22@gmail.com>
2024-03-22 21:04:53 +0330

    outsource description list in both links

---
+++ components/Intro/datis/datis-description.tsx
@@ -0,0 +1,18 @@
+export default function DatisDescriptionList() {
+  return (
+    <ul className="mt-4">
+    <li className="list-disc">
+      طراحی و ساخت شبیه سازهای آموزشی و تخصصی پزشکی
+    </li>
+    <li className="list-disc">
+      مشاوره و اجرا در زمینه تهیه تجهیزات پزشکی و آزمایشگاهی
+    </li>
+    <li className="list-disc">
+      تعمیر و نگهداری تجهیزات پزشکی، دندانپزشکی و آزمایشگاهی
+    </li>
+    <li className="list-disc">
+      ارائه انواع مواد مرجع و استاندارد (RM & CRM)
+    </li>
+  </ul>
+  )
+}
---
+++ components/Intro/persia/persia-description.tsx
@@ -0,0 +1,20 @@
+export default function PersiaDescriptionList() {
+  return (
+    <ul className="mt-4">
+      <li className="list-disc">
+        کالیبراسیون و کنترل کیفیت تجهیزات پزشکی، دندانپزشکی
+      </li>
+      <li className="list-disc">
+        کالیبراسیون و کنترل کیفیت تجهیزات آزمایشگاهی و فیزیوتراپی
+      </li>
+      <li className="list-disc">
+        کنترل کیفیت لیزرهای پزشکی (چشمی، پوستی و دندانی)
+      </li>
+      <li className="list-disc">کنترل کیفیت تجهیزات تصویربرداری پزشکی</li>
+      <li className="list-disc">کالیبراسیون تجهیزات ابزاردقیق و اندازه گیری</li>
+      <li className="list-disc">
+        برگزاری دوره های آموزشی تخصصی در حوزه اندازه شناسی و کالیبراسیون
+      </li>
+    </ul>
+  );
+}

ee8eafea93e73c657977a67c57ba5d84d8f49199
amirhosein <ah.t222amir22@gmail.com>
2024-05-08 18:45:09 +0330

    change some styles

--- components/Intro/datis/datis-link.tsx
+++ components/Intro/datis/datis-link.tsx
@@ -8,24 +8,19 @@ export default function DatisLink() {
   return (
     <Link
       href="/datis"
-      className="md:basis-1/2 rounded-lg shadow-lg hover:scale-105 transition-transform px-8 py-4 hover:border-gray-300 hover:bg-gray-100"
+      className="sm:basis-1/2 border min-h-72 border-[#D7D7D7] rounded-3xl  md:shadow-lg hover:scale-105 transition-transform px-8 py-6 hover:border-gray-300 hover:bg-gray-100"
     >
-      <div className="flex items-center gap-4">
-        <div>
-          <h2 className={`mb-3 text-2xl font-semibold`}>داتیس فناور</h2>
-          <p>شرکت تحقیقاتی تولیدی و مهندسی</p>
-        </div>
-        <Image
-          width={200}
-          height={200}
-          src={DatisLogo}
-          alt="datis logo"
-          priority
-        />
-      </div>
+      <Image
+        width={150}
+        height={150}
+        src={DatisLogo}
+        alt="datis logo"
+        priority
+      />
       <DatisDescriptionList />
-      <span>تلفن: </span>
-      <span>07132346668</span>
+      <span className="text-[10px] lg:text-[16px] inline-block mt-4">
+        تلفن: 07132346668
+      </span>
     </Link>
   );
 }
--- components/Intro/persia/persia-link.tsx
+++ components/Intro/persia/persia-link.tsx
@@ -8,23 +8,17 @@ export default function PersiaLink() {
   return (
     <Link
       href="/persia"
-      className="md:basis-1/2 rounded-lg shadow-lg hover:scale-105 transition-transform px-8 py-4 hover:border-gray-300 hover:bg-gray-100"
+      className="sm:basis-1/2 border border-[#D7D7D7] rounded-3xl md:shadow-lg hover:scale-105 transition-transform px-8 py-6 hover:border-gray-300 hover:bg-gray-100"
     >
-      <div className="flex items-center gap-4">
-        <div>
-          <h2 className={`mb-3 text-2xl font-semibold`}>پرشیا آزما سیستم</h2>
-          <p>شرکت تحقیقاتی آموزشی و خدماتی</p>
-        </div>
-        <Image
-          width={200}
-          height={200}
-          src={PersiaLogo}
-          alt="persia azma system logo"
-          priority
-        />
-      </div>
+      <Image
+        width={200}
+        height={200}
+        src={PersiaLogo}
+        alt="persia azma system logo"
+        priority
+      />
       <PersiaDescriptionList />
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-4 text-[10px] lg:text-[16px] mt-4">
         <div>
           <span>تلفن: </span>
           <span>07136245649</span>

c3dd1cac8b1fa089111a6c0626e7d67545b8568a
amirhosein <ah.t222amir22@gmail.com>
2024-06-11 14:26:00 +0330

    use feedback form in feedback page

---
+++ app/(companies)/persia/feedback/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import FeedbackImage from "@/public/images/feedback-image.png";
+import FeedbackForm from "@/components/feedback/feedback-form";
+
+export default function FeedbackPage() {
+  return (
+    <div className="container mx-auto px-12">
+      <Image
+        className="mx-auto w-1/2 max-h-[400px] object-contain"
+        src={FeedbackImage}
+        alt="feedback image"
+      />
+      <p className="text-center">
+        به منظور بهبود عملکرد شرکت لطفا نظر و پیشنهاد خود را برای ما ارسال کنید.
+      </p>
+      <FeedbackForm />
+    </div>
+  );
+}

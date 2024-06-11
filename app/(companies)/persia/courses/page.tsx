fabcaee6e387824708f3344291d3d763f5a63b4a
amirhosein <ah.t222amir22@gmail.com>
2024-06-11 14:26:26 +0330

    use CourseList in Courses page

---
+++ app/(companies)/persia/courses/page.tsx
@@ -0,0 +1,10 @@
+import CoursesList from "@/components/courses/courses-list";
+
+export default function Coursespage() {
+  return (
+    <div className="container mx-auto px-16">
+      <h2 className="font-bold">دوره ها</h2>
+      <CoursesList />
+    </div>
+  );
+}

1c5f4dc2a77f22f7410c4da3e67c8995df247c34
amirhosein <ah.t222amir22@gmail.com>
2024-06-24 11:05:32 +0330

    pass props to course box

--- components/courses/course-box.tsx
+++ components/courses/course-box.tsx
@@ -1,18 +1,30 @@
 import Image from "next/image";
 import CourseImage from "@/public/images/course-image.png";
+import { CourseViewModel } from "@/lib/interfaces/courses/CourseViewModel";
+import ShowCourseDescriptionButton from "./show-desc-btn";
+import CourseDescriptionDialog from "./course-description-dialog";
 
-export default function CourseBox() {
+export default function CourseBox({
+  title,
+  description,
+  duration,
+}: CourseViewModel) {
   return (
     <li className="card bg-secondary">
       <figure>
         <Image src={CourseImage} alt="عکس دوره" />
       </figure>
       <div className="card-body items-center text-center p-4">
-        <h4 className="card-title font-bold text-base">دوره تستی</h4>
-        <p>مدت دوره: 150 ساعت</p>
-        <p className="text-[#6B6B6B]">تاریخ شروع: 27 دی 1403</p>
+        <h4 className="card-title font-bold text-base">{title}</h4>
+        <p>مدت دوره: {duration} ساعت</p>
         <div className="card-actions mt-4">
-          <button className="btn btn-primary text-white text-xl font-normal px-8 ">
+          {description && (
+            <>
+              <ShowCourseDescriptionButton />
+              <CourseDescriptionDialog description={description} />
+            </>
+          )}
+          <button className="btn btn-primary text-white text-xl font-normal px-8">
             تماس
           </button>
         </div>

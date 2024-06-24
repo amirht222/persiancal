673d0fbc44448d389d2c57100b79e06787cc13e5
amirhosein <ah.t222amir22@gmail.com>
2024-06-24 11:05:59 +0330

    fetch courses and map on them

--- components/courses/courses-list.tsx
+++ components/courses/courses-list.tsx
@@ -1,47 +1,26 @@
+import { FetchResposne } from "@/lib/interfaces/fetch/FetchResponse";
 import CourseBox from "./course-box";
+import { getCourses } from "@/lib/actions/courses/coursesActions";
+import { CoursesSearchParams } from "@/lib/interfaces/courses/CoursesSearchParams";
+import { CourseViewModel } from "@/lib/interfaces/courses/CourseViewModel";
 
-const courses = [
-  {
-    id: 0,
-    title: "دوره کالیبراسیون",
-    duration: 150,
-    startDate: new Date(),
-  },
-  {
-    id: 1,
-    title: "دوره آشنایی با لوازم پزشکی",
-    duration: 120,
-    startDate: new Date(),
-  },
-  {
-    id: 2,
-    title: "دوره تستی",
-    duration: 70,
-    startDate: new Date(),
-  },
-];
-async function getCourses() {
-  const res = await fetch("http://localhost:3500/course/getList");
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
+interface Props {
+  searchParams: CoursesSearchParams;
 }
 
 export default async function CoursesList() {
-  const data = await getCourses();
-  console.log(data);
+  const {
+    count,
+    message,
+    ok,
+    res: courses,
+    status,
+  }: FetchResposne<CourseViewModel[]> = await getCourses();
 
   return (
     <ul className="mt-6 grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-24">
-      {courses.map((course) => (
-        <CourseBox key={course.id} />
-      ))}
+      {courses &&
+        courses.map((course) => <CourseBox {...course} key={course.id} />)}
     </ul>
   );
 }

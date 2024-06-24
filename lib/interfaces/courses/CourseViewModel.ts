d5f098c22b8bf70e4761f495635d7f04143ff335
amirhosein <ah.t222amir22@gmail.com>
2024-06-24 11:06:57 +0330

    create Course model interface

---
+++ lib/interfaces/courses/CourseViewModel.ts
@@ -0,0 +1,10 @@
+export interface CourseViewModel {
+  id?: string;
+  title?: string;
+  description?: string;
+  provider?: string;
+  duration?: number;
+  courseStatus?: number;
+  createdAt?: string;
+  updatedAt?: string;
+}

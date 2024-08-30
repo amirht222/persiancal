ac107949f1b50adf27d1f73013d06eb955e0c948
amirhosein <ah.t222amir22@gmail.com>
2024-08-30 18:00:46 +0330

    refactor and debug Header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,17 +1,17 @@
+import { CompanyName } from "@/lib/interfaces/general-types";
+import DatisLogo from "@/public/images/datis-logo.png";
+import PersiaLogo from "@/public/images/logo-persiaazma.png";
 import Image from "next/image";
 import Link from "next/link";
-import React, { Suspense } from "react";
-import PersiaLogo from "@/public/images/logo-persiaazma.png";
-import DatisLogo from "@/public/images/datis-logo.png";
-import { CompanyName } from "@/lib/interfaces/general-types";
-import NavItem from "./nav-item";
-import LabMenu from "./lab-menu";
+import ListItems from "./list-items";
 
 interface Props {
   companyName: CompanyName;
 }
 
-export default async function Header(props: Props) {
+export default function Header({ companyName }: Props) {
+  const lowerCaseCompanyName = companyName.toLowerCase();
+
   return (
     <header className="shadow-[0_0_10px_0_rgba(0,0,0,0.3)]">
       <div className="navbar">
@@ -37,48 +37,10 @@ export default async function Header(props: Props) {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <li>
-                <NavItem href="/">صفحه اصلی</NavItem>
-              </li>
-              <li>
-                <NavItem href="/persia">درباره ما</NavItem>
-              </li>
-              <li>
-                <Suspense fallback={<p>loading...</p>}>
-                  <LabMenu />
-                </Suspense>
-              </li>
-              <li>
-                <NavItem href="/persia/certificates">تاییدیه ها</NavItem>
-              </li>
-              <li>
-                <details>
-                  <summary>خدمات</summary>
-                  <ul className="p-2">
-                    <li>
-                      <NavItem href="/persia/courses">دوره ها</NavItem>
-                    </li>
-                    <li>
-                      <NavItem href="/persia/products">محصولات</NavItem>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-              <li>
-                <NavItem href="/persia/customer-services">خدمات مشتری</NavItem>
-              </li>
-              <li>
-                <NavItem href="/persia/feedback">نظرات و پیشنهاد ها</NavItem>
-              </li>
-              {/* <li>
-                <NavItem href="/persia/about">درباره ما</NavItem>
-              </li> */}
-              <li>
-                <a href="#footer">تماس با ما</a>
-              </li>
+              <ListItems companyName={lowerCaseCompanyName} />
             </ul>
           </div>
-          {props.companyName === "PERSIA" && (
+          {lowerCaseCompanyName === "persia" && (
             <Link href="/persia" className="btn btn-ghost text-xl">
               <Image
                 className="object-contain w-40"
@@ -88,7 +50,7 @@ export default async function Header(props: Props) {
               />
             </Link>
           )}
-          {props.companyName === "DATIS" && (
+          {lowerCaseCompanyName === "datis" && (
             <Link href="/datis" className="btn btn-ghost text-xl">
               <Image
                 className="object-contain w-28"
@@ -101,45 +63,7 @@ export default async function Header(props: Props) {
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li>
-              <NavItem href="/">صفحه اصلی</NavItem>
-            </li>
-            <li>
-              <NavItem href="/persia">درباره ما</NavItem>
-            </li>
-            <li>
-              <Suspense fallback={<p>loading...</p>}>
-                <LabMenu />
-              </Suspense>
-            </li>
-            <li>
-              <NavItem href="/persia/certificates">تاییدیه ها</NavItem>
-            </li>
-            <li>
-              <details>
-                <summary>خدمات</summary>
-                <ul className="p-2">
-                  <li>
-                    <NavItem href="/persia/courses">دوره ها</NavItem>
-                  </li>
-                  <li>
-                    <NavItem href="/persia/products">محصولات</NavItem>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <NavItem href="/persia/customer-services">خدمات مشتری</NavItem>
-            </li>
-            <li>
-              <NavItem href="/persia/feedback">نظرات و پیشنهاد ها</NavItem>
-            </li>
-            {/* <li>
-              <NavItem href="/persia/about">درباره ما</NavItem>
-            </li> */}
-            <li>
-              <a href="#footer">تماس با ما</a>
-            </li>
+            <ListItems companyName={lowerCaseCompanyName} />
           </ul>
         </div>
       </div>

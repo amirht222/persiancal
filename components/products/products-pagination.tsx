ef9488b9c8eb54c9ce3dac2f8305309d24af622a
amirhosein <ah.t222amir22@gmail.com>
2024-08-30 17:59:11 +0330

    initialize Pagination for Products

---
+++ components/products/products-pagination.tsx
@@ -0,0 +1,12 @@
+export default function ProductsPagination() {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="join">
+        <button className="join-item btn btn-primary text-white">1</button>
+        <button className="join-item btn btn-primary text-white">2</button>
+        <button className="join-item btn btn-primary text-white">3</button>
+        <button className="join-item btn btn-primary text-white">4</button>
+      </div>
+    </div>
+  );
+}

c4354b98e5fc7789c0e7d9486d758a354f47623b
amirhosein <ah.t222amir22@gmail.com>
2024-10-03 19:05:25 +0330

    add new params to course search params

--- lib/interfaces/courses/CoursesSearchParams.ts
+++ lib/interfaces/courses/CoursesSearchParams.ts
@@ -2,4 +2,6 @@ import { BaseSearchParams } from "../fetch/BaseSearchParams";
 
 export interface CoursesSearchParams extends BaseSearchParams {
   title?: string;
+  provider?: string;
+  courseStatus?: number;
 }

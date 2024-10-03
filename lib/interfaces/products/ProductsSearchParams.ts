dff9e6c4a41b96cf06b5b37fc29021e29085ba36
amirhosein <ah.t222amir22@gmail.com>
2024-10-03 19:05:12 +0330

    add provider and status to product params

--- lib/interfaces/products/ProductsSearchParams.ts
+++ lib/interfaces/products/ProductsSearchParams.ts
@@ -2,4 +2,6 @@ import { BaseSearchParams } from "../fetch/BaseSearchParams";
 
 export interface ProductsSearchParams extends BaseSearchParams {
   name?: string;
+  provider?: string;
+  productStatus?: number;
 }

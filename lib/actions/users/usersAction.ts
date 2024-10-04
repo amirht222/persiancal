e506c9b65c95ce946f4d8d92bdaee903ba36a1b9
sadEasterner <hoseinseifig1380@gmail.com>
2024-10-04 16:52:07 +0330

    [fix]: Fixed getUserInfo

--- lib/actions/users/usersAction.ts
+++ lib/actions/users/usersAction.ts
@@ -2,13 +2,16 @@
 
 import { baseFetch } from "@/lib/baseFetch/baseFetch";
 import { FetchResposne } from "@/lib/interfaces/fetch/FetchResponse";
+import { cookies } from "next/headers";
 
 const apiURL: string = process.env.API_URL!;
 
 const getUserInfo = async () => {
   const endpoint = "user/getByUsername";
   const method = "GET";
-  return await baseFetch(apiURL, endpoint, method);
+  const token = cookies().get("token");
+  const response = await baseFetch(apiURL, endpoint, method);
+  return { authorized: !!token, ...response };
 };
 
 export { getUserInfo };

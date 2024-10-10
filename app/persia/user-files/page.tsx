acfe97d8cab57b3d4053775b87851f27d60afd55
amirhosein <ah.t222amir22@gmail.com>
2024-10-10 15:12:36 +0330

    call getUserInfo

--- app/persia/user-files/page.tsx
+++ app/persia/user-files/page.tsx
@@ -1,4 +1,7 @@
-export default function UserFilesPage() {
-    
+import { getUserInfo } from "@/lib/actions/users/usersAction";
+
+export default async function UserFilesPage() {
+  const res = await getUserInfo();
+
   return <div>your files!</div>;
 }

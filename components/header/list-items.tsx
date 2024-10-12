c37a1723e6cd369f7919aa54b797f4c5169ca03f
amirhosein <ah.t222amir22@gmail.com>
2024-10-12 19:27:05 +0330

    add new page links to header

--- components/header/list-items.tsx
+++ components/header/list-items.tsx
@@ -3,50 +3,59 @@ import NavItem from "./nav-item";
 import LabMenu from "./lab-menu";
 import CustomerServiceBtn from "./customer-service-btn";
 import CustomerLoginModal from "./CustomerLoginModal";
+import { Provider } from "@/lib/interfaces/general-types";
+import Spinner from "../loading";
 
-export default function ListItems({ companyName }: { companyName: string }) {
+export default function ListItems({ provider }: { provider: Provider }) {
   return (
     <>
       <li>
         <NavItem href="/">صفحه اصلی</NavItem>
       </li>
       <li>
-        <NavItem href={`/${companyName}`}>درباره ما</NavItem>
+        <NavItem href={`/${provider}`}>درباره ما</NavItem>
       </li>
-      {/* {companyName === "persia" && (
+      {provider === "persia" && (
         <li>
           <Suspense
             fallback={
-              <span className="loading loading-spinner loading-md"></span>
+              // <span className="loading loading-spinner loading-md"></span>
+              <Spinner />
             }
           >
             <LabMenu />
           </Suspense>
         </li>
-      )} */}
+      )}
       <li>
-        <NavItem href={`/${companyName}/certificates`}>تاییدیه ها</NavItem>
+        <NavItem href={`/${provider}/certificates`}>تاییدیه ها</NavItem>
       </li>
       <li>
         <details>
           <summary>خدمات</summary>
           <ul className="p-2">
             <li>
-              <NavItem href={`/${companyName}/courses`}>دوره ها</NavItem>
+              <NavItem href={`/${provider}/courses`}>دوره ها</NavItem>
             </li>
             <li>
-              <NavItem href={`/${companyName}/products`}>محصولات</NavItem>
+              <NavItem href={`/${provider}/products`}>محصولات</NavItem>
+            </li>
+            <li>
+              <NavItem href={`/${provider}/consultations`}>مشاوره</NavItem>
             </li>
           </ul>
         </details>
       </li>
-      {companyName === "persia" && (
+      {provider === "persia" && (
         <li>
           <CustomerServiceBtn />
         </li>
       )}
       <li>
-        <NavItem href={`/${companyName}/feedback`}>نظرات و پیشنهاد ها</NavItem>
+        <NavItem href={`/${provider}/articles`}>مقالات</NavItem>
+      </li>
+      <li>
+        <NavItem href={`/${provider}/feedback`}>نظرات و پیشنهاد ها</NavItem>
       </li>
       <li>
         <a href="#footer">تماس با ما</a>

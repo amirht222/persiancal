71d7f3d1a0bdcc51386c9f78f0ebcde1284bcf6b
amirhosein <ah.t222amir22@gmail.com>
2024-10-12 19:25:22 +0330

    create a global spinner loading

---
+++ components/loading/index.tsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <span className="loading loading-spinner text-primary loading-lg"></span>
+    </div>
+  );
+}

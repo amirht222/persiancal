2ee14d41a1d6beb065ebdf50ccb31fe37cec1c00
amirhosein <ah.t222amir22@gmail.com>
2024-10-16 14:51:45 +0330

    check provider and redirect

--- app/[provider]/layout.tsx
+++ app/[provider]/layout.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/header";
 import Spinner from "@/components/loading";
 import { Provider } from "@/lib/interfaces/general-types";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
 export default function ProviderLayout({
@@ -11,10 +12,14 @@ export default function ProviderLayout({
   children: React.ReactNode;
   params: { provider: Provider };
 }>) {
+  if (params.provider !== "persia" && params.provider !== "datis") {
+    redirect("/");
+  }
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header provider={params.provider} />
-      <main className="grow my-6">{children}</main>
+      <main className="grow my-6 min-h-[50vh]">{children}</main>
       <Suspense fallback={<Spinner />}>
         <Footer provider={params.provider} />
       </Suspense>

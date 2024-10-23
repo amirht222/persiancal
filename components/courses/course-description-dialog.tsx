29aa35c213976f042e405ee8b1198dc88caf9ee3
amirhosein <ah.t222amir22@gmail.com>
2024-10-23 18:49:46 +0330

    fix course description display

--- components/courses/course-description-dialog.tsx
+++ components/courses/course-description-dialog.tsx
@@ -1,12 +1,19 @@
 interface Props {
-  description: string;
+  description?: string;
 }
 
 export default function CourseDescriptionDialog({ description }: Props) {
   return (
     <dialog id="course_description_modal" className="modal">
       <div className="modal-box">
-        <p className="py-4">{description}</p>
+        {description && (
+          <div
+            className="py-4"
+            dangerouslySetInnerHTML={{
+              __html: description,
+            }}
+          />
+        )}
         <div className="modal-action">
           <form method="dialog">
             <button className="btn btn-primary text-white">بستن</button>

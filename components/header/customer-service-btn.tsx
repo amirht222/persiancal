0cf42bee7678f698d2d5900cd407868e3e3e54a2
amirhosein <ah.t222amir22@gmail.com>
2024-10-23 19:56:27 +0330

    debug

--- components/header/customer-service-btn.tsx
+++ components/header/customer-service-btn.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-
 export default function CustomerServiceBtn() {
   return (
-    <button
-      onClick={() =>
-        (
-          document.getElementById("customer_login_modal") as HTMLDialogElement
-        ).showModal()
-      }
-    >
-      خدمات مشتری
-    </button>
+    <>
+      <button
+        onClick={() =>
+          (
+            document.getElementById("customer_login_modal") as HTMLDialogElement
+          ).showModal()
+        }
+      >
+        خدمات مشتری
+      </button>
+    </>
   );
 }

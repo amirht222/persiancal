cd872ab6002df6da4ef39ce70b6e9a630d06e375
amirhosein <ah.t222amir22@gmail.com>
2024-10-23 19:54:53 +0330

    change intro links styles

--- components/Intro/ProviderLinkBox.tsx
+++ components/Intro/ProviderLinkBox.tsx
@@ -11,20 +11,26 @@ export default function ProviderLinkBox(props: ProviderViewModel) {
       className="sm:basis-1/2 border min-h-72 border-[#D7D7D7] rounded-3xl  md:shadow-lg hover:scale-105 transition-transform px-8 py-6 hover:border-gray-300 hover:bg-gray-100"
     >
       <Image
-        width={props.providerTitle === "persia" ? 200 : 150}
-        height={props.providerTitle === "persia" ? 200 : 150}
+        width={props.providerTitle === "persia" ? 220 : 150}
+        height={props.providerTitle === "persia" ? 220 : 150}
         src={props.providerTitle === "persia" ? PersiaLogo : DatisLogo}
         alt={`${props.providerTitle} logo`}
         priority
+        className="mx-auto"
       />
-      <ul className="mt-4 *:mt-4 text-[10px] lg:text-[16px] list-image-[url(/images/list-bullet.png)]">
-        {props.providerActivities?.slice(0, 4).map((activity) => (
+      <ul className="mt-4 space-y-4 text-[14px] lg:text-[18px] list-image-[url(/images/list-bullet.png)]">
+        {props.providerActivities?.slice(0, 6).map((activity) => (
           <li key={activity.id}>{activity.text}</li>
         ))}
       </ul>
-      <span className="text-[10px] lg:text-[16px] inline-block mt-4">
-        تلفن: {props.telephone}
-      </span>
+      <div className="mt-4 flex items-center gap-4">
+        <span className="text-[12px] lg:text-[16px] inline-block">
+          تلفن: {props.telephone}
+        </span>
+        <span className="text-[12px] lg:text-[16px] inline-block">
+          فکس: {props.fax}
+        </span>
+      </div>
     </Link>
   );
 }

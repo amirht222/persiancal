69f1a38aa72099897e5686f5edc9d743ca756b9f
amirhosein <ah.t222amir22@gmail.com>
2024-10-23 19:55:21 +0330

    change provider hero

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -12,22 +12,28 @@ interface Props {
 export default function ProviderHero(props: Props) {
   return (
     <>
-      <div className="flex justify-between items-center gap-4 mt-4 lg:mt-6">
-        <div>
-          <h2 className="font-bold text-2xl lg:text-[50px]">
-            معرفی {providerTitleMapper(props.providerData.providerTitle)}
-          </h2>
-          <p className="text-[10px] lg:text-base mt-4 lg:mt-[60px] text-justify">
-            {props.providerData.aboutUs}
-          </p>
+      <div className="container w-full  md:w-2/3 mx-auto mt-4 lg:mt-12">
+        <h2 className="font-bold text-2xl lg:text-[50px] text-center">
+          معرفی {providerTitleMapper(props.providerData.providerTitle)}
+        </h2>
+        {/* <p className="">
+          {props.providerData.aboutUs}
+        </p> */}
+        <div
+          className="text-[10px] lg:text-base mt-4 lg:mt-[60px]"
+          dangerouslySetInnerHTML={{
+            __html: (props.providerData as any).aboutUs,
+          }}
+        />
+        <div className="flex justify-center items-center">
           <ShowMoreButton />
         </div>
-        <Image
+        {/* <Image
           src={HeroImage}
           alt="hero image"
           priority
           className="object-cover w-[400px] h-44 lg:h-auto lg:mx-14"
-        />
+        /> */}
       </div>
       <DetailsDialog providerData={props.providerData} />
     </>
